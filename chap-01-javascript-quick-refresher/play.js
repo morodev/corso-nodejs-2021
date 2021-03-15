d6e0e659cd779692ed9cd44efe6f376faef2703b
Luca Moro <71976553+morodev@users.noreply.github.com>
2021-03-15 22:25:50 +0100

    utilizzo di let e const per le variabili

--- chap-01-javascript-quick-refresher/play.js
+++ chap-01-javascript-quick-refresher/play.js
@@ -1,7 +1,9 @@
-var name = 'luca moro';
-var age = 32;
+const name = 'luca moro';
+let age = 32;
 var work = 'Front-End Developer - Angular specialist';
 var hasMarried = false;
+
+age = 31;
 
 function printPerson(name, age, work, hasMarried) {
     return ("Ciao, io sono " + name + ", ed ho " + age + " anni, la mia professione è " + work + ", sono sposato?" + hasMarried);
